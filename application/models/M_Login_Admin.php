95da7da6912ea286463464cbfcc9d577f0040fac
WildanRaffians <134181656+WildanRaffians@users.noreply.github.com>
2023-06-20 10:36:26 +0700

    nambah fitur register

--- application/models/M_Login_Admin.php
+++ application/models/M_Login_Admin.php
@@ -9,7 +9,7 @@ class M_Login_Admin extends CI_Model
     }
 
     function getAdmin($username){
-        $this->db->select('id_admin','username', 'moto_admin', 'Role', 'jenis_kelamin', 'email');
+        $this->db->select('id_admin','username', 'password', 'moto_admin', 'Role', 'jenis_kelamin', 'email');
         $this->db->from('t_admin');
         // $this->db->join('t_role', 't_admin.id_role = t_role.id_role');
 		// $this->db->join('t_jenis_kelamin', 't_admin.id_jenis_kelamin = t_jenis_kelamin.id_jenis_kelamin');
